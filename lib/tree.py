65e7a1d1aed101fa8f4740a63ca333b2aacd5e5b
Sundeep <sundeep.kaler@gmail.com>
2024-09-20 01:21:42 -0400

    complete

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,15 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    if self.root is None:
+      return None
+
+    if self.root['id'] == id:
+        return self.root
+
+    for child in self.root['children']:
+        result = Tree(child).get_element_by_id(id)
+        if result:
+            return result
+
+    return None
